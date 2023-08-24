c5416b8bad045ae84095200eb12c425cc2f4df0c
cmem <mesado31@gmail.com>
2023-08-24 23:03:34 +0200

    gravitational potential energy

---
+++ equations/gravitational_potential_energy_law.py
@@ -0,0 +1,40 @@
+from scipy.constants import G
+
+from ..drivers.axes import Axes
+from ..drivers.equation import Equation
+from ..utils import distance
+
+
+class GravitationalPotentialEnergyLaw:
+    
+    def __init__(self, filed):
+
+        self.field = filed
+        self.axes = Axes(self.field.dimensions)
+
+    def _equation(self, main_body, axis):
+
+        foo = 0.0
+        axis = self.axes.components[axis]
+
+        # equation to solve is Ep + G*M*Sum mi/d  = 0
+        
+        for body in self.field.bodies:
+            if body is not main_body:
+                
+                dist = distance(main_body.position.value, body.position.value)
+                foo += body.mass.value/dist
+
+        foo *= G*main_body.mass.value
+        foo += main_body.gravitational_potential_energy.value
+        
+        return Equation(foo)
+    
+    def solve(self, main_body, unknown, axis):
+
+        if axis not in self.axes.components.keys():
+            raise ValueError(f'Parameter \'axis\' must be one of these {self.axes.components.keys()}')
+
+        equation = self._equation(main_body, axis)
+
+        return equation.solve(unknown)
--- tests/test_gravity.py
+++ tests/test_gravity.py
@@ -8,10 +8,10 @@ from physics.drivers.field import Field
 from physics.utils import compare_floats
 
 
-def test_gravity_b1_2019_junio():
+def test_gravity_b1_2019_junio_a():
     """
     F2-PAU-Gravitation
-    B1 2019 junio
+    B1.a 2019 junio
     """
     body_a = Body(name='A')
     body_a.set('mass', 3)
@@ -25,8 +25,34 @@ def test_gravity_b1_2019_junio():
     field.add_body(body_a)
     field.add_body(body_b)
 
-    Fg_solve = field.solve_gravitation_equation('A', 'Fg')
+    Fg_solve = field.solve_gravitational_force_equation('A', 'Fg')
     Fg_get = field.get_gravitational_force_over('A')
 
     assert compare_floats(Fg_solve[0], -1.25E-10)
     assert compare_floats(Fg_get, -1.25E-10)
+
+def test_gravity_b1_2019_junio_b():
+    """
+    F2-PAU-Gravitation
+    B1.b 2019 junio
+    """
+    # TODO review methodology
+    body_b = Body(name='B')
+    body_b.set('mass', 5)
+    body_b.set('position', (2, -2))
+
+    body_bp = Body(name='Bp')
+    body_bp.set('mass', 5)
+    body_bp.set('position', (2, 0))
+
+    field = Field()
+    field.add_body(body_b)
+    field.add_body(body_bp)
+
+    Ep_b_solve = field.solve_gravitational_potential_energy_equation('B', 'Epg')
+    Ep_bp_solve = field.solve_gravitational_potential_energy_equation('Bp', 'Epg')
+    Ep_b_get = field.get_gravitational_potential_energy_over('B')
+    Ep_bp_get = field.get_gravitational_potential_energy_over('Bp')
+
+    assert compare_floats(Ep_b_solve[0]-Ep_bp_solve[0], 1.47E-10)
+    assert compare_floats(Ep_b_get-Ep_bp_get, 1.47E-10)
