0f234e205a6c4968a2556fff88092d6344728be2
cmem <mesado31@gmail.com>
2023-08-24 23:03:22 +0200

    gravitational force law

--- equations/gravitational_force_law.py
+++ equations/gravitational_force_law.py
@@ -5,14 +5,14 @@ from ..drivers.equation import Equation
 from ..utils import distance
 
 
-class GravitationLaw:
+class GravitationalForceLaw:
     
     def __init__(self, filed):
 
         self.field = filed
         self.axes = Axes(self.field.dimensions)
 
-    def _equation(self, _body, axis):
+    def _equation(self, main_body, axis):
 
         foo = 0.0
         axis = self.axes.components[axis]
@@ -20,21 +20,21 @@ class GravitationLaw:
         # equation to solve is Fab + G*M*Sum mi/d**2  = 0
         
         for body in self.field.bodies:
-            if body is not _body:
+            if body is not main_body:
                 
-                dist = distance(_body.position.value, body.position.value)
+                dist = distance(main_body.position.value, body.position.value)
                 foo += body.mass.value/dist**2
 
-        foo *= G*_body.mass.value
-        foo += _body.gravity_force.value
+        foo *= G*main_body.mass.value
+        foo += main_body.gravitational_force.value
         
         return Equation(foo)
     
-    def solve(self, _body, unknown, axis):
+    def solve(self, main_body, unknown, axis):
 
         if axis not in self.axes.components.keys():
             raise ValueError(f'Parameter \'axis\' must be one of these {self.axes.components.keys()}')
 
-        equation = self._equation(_body, axis)
+        equation = self._equation(main_body, axis)
 
         return equation.solve(unknown)
