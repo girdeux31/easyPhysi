2c37188694843d27e7cf01b9bf727ae4225a4823
cmem <mesado31@gmail.com>
2023-09-04 00:06:42 +0200

    gravitational test

--- tests/test_gravity.py
+++ tests/test_gravity.py
@@ -36,23 +36,67 @@ def test_gravity_b1_2019_junio_b():
     F2-PAU-Gravitation
     B1.b 2019 junio
     """
-    # TODO review methodology
     body_b = Body(name='B')
     body_b.set('mass', 5)
     body_b.set('position', (2, -2))
 
-    body_bp = Body(name='Bp')
-    body_bp.set('mass', 5)
-    body_bp.set('position', (2, 0))
-
     field = Field()
     field.add_body(body_b)
-    field.add_body(body_bp)
 
     Ep_b_solve = field.solve_gravitational_potential_energy_equation('B', 'Epg')
-    Ep_bp_solve = field.solve_gravitational_potential_energy_equation('Bp', 'Epg')
     Ep_b_get = field.get_gravitational_potential_energy_over('B')
-    Ep_bp_get = field.get_gravitational_potential_energy_over('Bp')
+
+    body_b.set('position', (2, 0))
+    
+    Ep_bp_solve = field.solve_gravitational_potential_energy_equation('B', 'Epg')
+    Ep_bp_get = field.get_gravitational_potential_energy_over('B')
 
     assert compare_floats(Ep_b_solve[0]-Ep_bp_solve[0], 1.47E-10)
     assert compare_floats(Ep_b_get-Ep_bp_get, 1.47E-10)
+
+def test_gravity_a1_2019_junio_a1():
+    """
+    F2-PAU-Gravitation
+    A1.a1 2019 junio
+    """
+    body_a = Body(name='A')
+    body_a.set('mass', 5)
+    body_a.set('position', (4, 3))
+    point = (0, 0)
+
+    field = Field()
+    field.add_body(body_a)
+
+    g_solve = field.solve_gravitational_field_intensity_equation(point, 'gg')
+    g_get = field.get_gravitational_field_intensity_in(point)
+    
+    assert compare_floats(g_solve[0], -1.33E-11)
+    assert compare_floats(g_get, -1.33E-11)
+
+def test_gravity_b1_2022_model_b():
+    """
+    F2-PAU-Gravitation
+    B1.b 2022 model
+    """
+    ma = 1
+    mb = 10
+    g = -9.81
+    pb = (0, 0)
+    pa_x = 0
+
+    body_a = Body(name='A')
+    body_a.set('mass', ma)
+    body_a.set('position', pa_x, axis='x')
+    body_a.set('gravitational_force', ma*g*1E-9)
+
+    body_b = Body(name='B')
+    body_b.set('mass', mb)
+    body_b.set('position', pb)
+
+    field = Field()
+    field.add_body(body_a)
+    field.add_body(body_b)
+
+    p_solve = field.solve_gravitational_force_equation('A', 'p')
+
+    assert compare_floats(p_solve[1], 0.26)
