db90a7e4439274125f8cdc62c07847116390c75d
cmem <mesado31@gmail.com>
2023-10-26 18:52:01 +0200

    feat: body.set/unset w short instead of long name

--- pyphysics/drivers/body.py
+++ pyphysics/drivers/body.py
@@ -8,8 +8,8 @@ class Body:
 
     def __init__(self, name, dimensions=2):
 
-        if dimensions < 1 or dimensions > 3:
-            raise ValueError('Parameter \'dimensions\' must be between 1 and 3')
+        if dimensions != 2 and dimensions != 3:
+            raise ValueError('Parameter \'dimensions\' must be 2 or 3')
         
         self.name = name
         self.dimensions = dimensions
@@ -17,7 +17,7 @@ class Body:
         self.forces = list()
         self.energies = list()
 
-        # initialize parameters
+        # initialize properties
 
         self.acceleration = Vector('a', self.dimensions)
         self.charge = Scalar('q')
@@ -37,28 +37,56 @@ class Body:
         self.time = Scalar('t')
         self.velocity = Vector('v', self.dimensions)
 
-    def set(self, parameter, value, axis=None):
+    def short_to_long(self, short):
 
-        if not hasattr(self, parameter):
+        for long, obj in self.__dict__.items():
+
+            if isinstance(obj, (Vector, Scalar)):
+                if obj.name == short:
+                    return long
+
+        return None
+
+    def set(self, name, value):
+
+        axis = None
+
+        # get axis and remove it from name
+
+        if name.endswith(('_x', '_y', '_z')):
+            name, axis = name[:-2], name[-1]
+
+        property_ = self.short_to_long(name)
+
+        if not hasattr(self, property_):
 
             self.help()
-            raise ValueError(f'Parameter \'{parameter}\' not found, see recognized parameters in table above')
+            raise ValueError(f'Property \'{property_}\' not found, see allowed properties in table above')
         
-        unknown = getattr(self, parameter)
+        unknown = getattr(self, property_)
 
         if isinstance(unknown, Vector):
             unknown.define(value, axis=axis)
         else:
             unknown.define(value)
 
-    def unset(self, parameter, axis=None):
+    def unset(self, name):
 
-        if not hasattr(self, parameter):
+        axis = None
+
+        # get axis and remove it from name
+
+        if name.endswith(('_x', '_y', '_z')):
+            name, axis = name[:-2], name[-1]
+
+        property_ = self.short_to_long(name)
+
+        if not hasattr(self, property_):
 
             self.help()
-            raise ValueError(f'Parameter \'{parameter}\' not found, see recognized parameters in table above')
+            raise ValueError(f'Property \'{property_}\' not found, see allowed properties in table above')
         
-        unknown = getattr(self, parameter)
+        unknown = getattr(self, property_)
         
         if isinstance(unknown, Vector):
             unknown.undefine(axis=axis)
@@ -78,27 +106,31 @@ class Body:
     def help(self):
         #TODO what about force and energy?
 
-        print('The following parameters are recognized:')
         print('')
-        print(' {:32s} {:8s} {:10s} {:16s}'.format('Parameter', 'Unknown', 'Type', 'Value'))
-        print(' ' + '='*32 + ' ' + '='*8 + ' '+ '='*10 + ' '+ '='*16)
+        print('The following properties are allowed:')
+        print('')
+        print(' {:10s} {:32s} {:10s} {:16s}'.format('Property', 'Description', 'Type', 'Value'))
+        print(' ' + '='*10 + ' ' + '='*32 + ' '+ '='*10 + ' '+ '='*16)
 
         for key, value in self.__dict__.items():
 
             if isinstance(value, (Vector, Scalar)):
-                print(f' {key:32s} {value.name:8s} {type(value).__name__:10s} {value.value}')
+                description = key[0].upper() + key[1:].replace('_', ' ')
+                print(f' {value.name:10s} {description:32s} {type(value).__name__:10s} {value.value}')
 
         print('')
 
 
 electron = Body('electron')
-electron.set('mass', electron_mass)
-electron.set('charge', -elementary_charge)
+electron.set('m', electron_mass)
+electron.set('q', -elementary_charge)
 
 proton = Body('proton')
-proton.set('mass', proton_mass)
-proton.set('charge', +elementary_charge)
+proton.set('m', proton_mass)
+proton.set('q', +elementary_charge)
 
 neutron = Body('neutron')
-neutron.set('mass', neutron_mass)
-neutron.set('charge', 0.0)
+neutron.set('m', neutron_mass)
+neutron.set('q', 0.0)
+
+# TODO include earth moon jupiter etc
