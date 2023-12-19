47273d48ae5c36ed529d05f37db29216851f1a6a
cmem <mesado31@gmail.com>
2023-12-20 00:30:30 +0100

    test: add test in 3d for EF

--- tests/test_gravitational.py
+++ tests/test_gravitational.py
@@ -34,6 +34,32 @@ def test_gravity_b1a_2019_junio_coincidentes():
     assert compare_floats(Fg_y, +8.84E-11)
     assert compare_floats(Fg, +1.25E-10)
 
+def test_gravity_b1a_2019_junio_coincidentes_3d():
+    """
+    URL: https://gitlab.com/fiquipedia/drive.fiquipedia/-/raw/main/content/home/recursos/recursospau/ficherospaufisicaporbloques/F2-PAU-Gravitacion.pdf
+    Problem: B1.a 2019 junio
+    Statement: A point mass A, MA = 3 kg, is located on the xy-plane, at the origin of coordinates. If a point mass B, MB = 5 kg, is placed at point (2, -2, 0) m, determine:
+    a) The force exerted by mass A on mass B.
+    """
+    body_a = Body('A', dimensions=3)
+    body_a.set('m', 3)
+    body_a.set('p', (0, 0, 0))
+
+    body_b = Body('B', dimensions=3)
+    body_b.set('m', 5)
+    body_b.set('p', (2, -2, 0))
+
+    universe = Universe(dimensions=3)
+    universe.add_body(body_a)
+    universe.add_body(body_b)
+
+    Fg_x, Fg_y = universe.gravitational_force_equation('B').solve(['Fg_x', 'Fg_y'])
+    Fg = magnitude((Fg_x, Fg_y))  # always positive value
+
+    assert compare_floats(Fg_x, -8.84E-11)
+    assert compare_floats(Fg_y, +8.84E-11)
+    assert compare_floats(Fg, +1.25E-10)
+
 def test_gravity_b1b_2019_junio_coincidentes():
     """
     URL: https://gitlab.com/fiquipedia/drive.fiquipedia/-/raw/main/content/home/recursos/recursospau/ficherospaufisicaporbloques/F2-PAU-Gravitacion.pdf
