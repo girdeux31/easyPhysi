51e28e46e97fa65e060419b1af34e335e78fce3c
cmem <mesado31@gmail.com>
2023-12-22 18:15:54 +0100

    chore: v1.0.1, add keywords

--- easyphysi/__init__.py
+++ easyphysi/__init__.py
@@ -1,5 +1,5 @@
 __name__ = 'easyphysi'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __summary__ = 'Physics library to solve pre-universitary physics problems'
 __author__ = 'Carles Mesado'
 __email__ = 'mesado31@gmail.com'
